7ef683f0ff36911ac31c241e6bb66a9f9ac7e9d9
aguaimale <agustinaimale@gmail.com>
2025-10-07 11:13:15 -0300

    Add force-static configuration for sitemap and robots

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { sitemapConfig } from '@/lib/seo';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
    return sitemapConfig.routes.map((route) => ({
       url: `${sitemapConfig.hostname}${route.url}`,
